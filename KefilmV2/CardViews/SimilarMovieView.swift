ab307b18764306ab8530cb8d357e8a4c668526dc
Oğuz Demirhan <squiretoss17@gmail.com>
2021-08-31 23:24:16 +0300

    Similar Movie Viewmodel added

---
+++ KefilmV2/CardViews/SimilarMovieView.swift
@@ -0,0 +1,20 @@
+//
+//  SimilarMovieView.swift
+//  KefilmV2
+//
+//  Created by Oguz Demırhan on 31.08.2021.
+//
+
+import SwiftUI
+
+struct SimilarMovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SimilarMovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        SimilarMovieView()
+    }
+}
